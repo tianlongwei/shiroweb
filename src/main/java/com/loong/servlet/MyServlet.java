689cc7bdc26e2aa14c5b8767758e5ea9b4276fdc
tianlongwei <tianan_long_wei@163.com>
2019-06-18 18:22:49 +0800

    shiro与servlet整合

--- src/main/java/com/loong/servlet/MyServlet.java
+++ src/main/java/com/loong/servlet/MyServlet.java
@@ -1,7 +1,13 @@
 package com.loong.servlet;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class MyServlet implements Servlet {
@@ -24,10 +30,26 @@ public class MyServlet implements Servlet {
         System.out.println(this.getClass().getName()+"---service");
         //处理登录
         HttpServletRequest request= (HttpServletRequest) servletRequest;
+        HttpServletResponse response= (HttpServletResponse) servletResponse;
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         System.out.println(username);
         System.out.println(password);
+
+        //获取当前登录对象
+        Subject subject = SecurityUtils.getSubject();
+        if (!subject.isAuthenticated()){
+            UsernamePasswordToken token=new UsernamePasswordToken(username,password);
+            try {
+                subject.login(token);
+                response.sendRedirect("/list.jsp");
+            }catch (AccountException e){
+                System.out.println(e.getMessage());
+                response.sendRedirect("/login.jsp");
+                return;
+            }
+        }
     }
 
     @Override
---
+++ src/main/webapp/list.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2019/6/18
+  Time: 17:32
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>list</title>
+</head>
+<body>
+    list aaa
+    <a href="/logout">登出</a>
+</body>
+</html>
